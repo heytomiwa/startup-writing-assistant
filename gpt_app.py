650d9b647fa46268141bde78c7e8b035db72c7c9
ihatetommy <tommypinsel@gmail.com>
2023-03-03 22:59:25 +0100

    update

--- gpt_app.py
+++ gpt_app.py
@@ -1,6 +1,6 @@
 import main
 import streamlit as st
 
-st.set_pagge_config(page_title="Startup Writing Assistant", page_icon=":shark:", layout="wide")
+st.set_page_config(page_title="Startup Writing Assistant", page_icon=":shark:", layout="wide")
 
 main.app()
